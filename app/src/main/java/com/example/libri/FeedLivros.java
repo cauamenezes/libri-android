2bc7fde20b09f4b72e64dbd62f811c85ce2fcb6e
Cauã Menezes <79976791+cauamenezes@users.noreply.github.com>
2022-03-04 10:15:25 -0800

    criação dos métodos de adapter e finalização do recycler view

--- app/src/main/java/com/example/libri/FeedLivros.java
+++ app/src/main/java/com/example/libri/FeedLivros.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import database.SQLHelper;
 import model.Item;
 import model.Livro;
 
@@ -24,6 +26,12 @@ public class FeedLivros extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_livros);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        List<Item> item = SQLHelper.getInstance(this).listBook();
+
+        recyclerView.setAdapter(new LivroAdapter(item));
+
     }
 
     /** INFLATE DO MENU **/
@@ -88,7 +96,10 @@ public class FeedLivros extends AppCompatActivity {
 
             if (viewType == 0){
 
-                return new LivroAdapter.LivroViewHolder();
+                return new LivroAdapter.LivroViewHolder(
+                        LayoutInflater.from(parent.getContext())
+                                .inflate(R.layout.item_container_livro, parent, false)
+                );
 
             }
 
@@ -99,11 +110,28 @@ public class FeedLivros extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
 
+           if(getItemViewType(position) == 0) {
+
+               //TIPO LIVRO
+            Livro livro = (Livro) item.get(position).getObject();
+               ((LivroAdapter.LivroViewHolder) holder).setLivroData(livro);
+
+           }
+            // else if (getItemViewType(position) == 0){
+
+               //TIPO HQ
+           //}
+
+        }
+
+        /** MÉTODO AUXILIAR DE MANIPULAÇÃO DE POSITION PARA O MÉTODO onBindViewHolder **/
+        public int getItemViewType(int position) {
+            return item.get(position).getType();
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return item.size();
         }
 
         /** VIEWHOLDER **/
